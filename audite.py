7f5ad388627e9862866eebfb9cd3a8adb10dee97
Taylor Seale <tseale@nd.edu>
2013-04-21 16:13:01 -0400

    Submitting search shows 3 similar artists and their pics
    -All done using AJAX
    -Need to format the pics so that they are the same size

--- audite.py
+++ audite.py
@@ -18,7 +18,10 @@ class Audite(object):
     def submit(self,search_artist):
         cherrypy.response.headers['Content-Type'] = 'application/json'
         similarArtists=artist.similar(names=artist.Artist(search_artist).name,results=3)
-        return simplejson.dumps(dict(simArtist1=similarArtists[0].name,simArtist2=similarArtists[1].name,simArtist3=similarArtists[2].name))
+        image1=similarArtists[0].get_images(results=1)
+        image2=similarArtists[1].get_images(results=1)
+        image3=similarArtists[2].get_images(results=1)
+        return simplejson.dumps(dict(simArtist1Name=similarArtists[0].name,simArtist1Image=image1[0]['url'],simArtist2Name=similarArtists[1].name,simArtist2Image=image2[0]['url'],simArtist3Name=similarArtists[2].name,simArtist3Image=image3[0]['url']))
 
 config = {'/html':
                 {'tools.staticdir.on': True,
