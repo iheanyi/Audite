5b4998fa2873a31f64eae95771eb06aa88cfaac2
Iheanyi Ekechukwu <iekechukwu@gmail.com>
2013-04-24 02:04:18 -0400

    Heroku junts?

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: python audite.py
--- audite.py
+++ audite.py
@@ -327,6 +327,12 @@ config = {'/html':
                 {
                 'server.socket_host':'0.0.0.0',
                 },
+
+
         }
+
+cherrypy.config.update({'server.socket_host': '0.0.0.0',})
+cherrypy.config.update({'server.socket_port': int(os.environ.get('PORT', '5000')),})
+
 
 cherrypy.quickstart(Audite(), '/', config=config)
